bd456031b02cd0dbd3af4e05b3f21cc5565c19a9
LAK132 <LAK132@kicodora.com>
2018-05-02 22:05:38 +0930

    Fix for nullptr_t support

--- laktype.hpp
+++ laktype.hpp
@@ -2,9 +2,11 @@
 using std::shared_ptr;
 using std::make_shared;
 #include <type_traits>
-using std::remove_reference;
+using std::remove_reference_t;
 #include <exception>
 using std::exception;
+#include <cstddef>
+using std::nullptr_t;
 
 #ifndef funcptr_t
 #define funcptr_t uint64_t
@@ -15,13 +17,32 @@ using std::exception;
 
 class lak_type_exception : public exception { using exception::exception; };
 
+struct void_t {};
+
 template<typename T>
-T typeof_f(T t) { return t; }
+void typeof_f(T t){}
 
 template<typename T>
 struct typeof 
 {
-    funcptr_t type = (funcptr_t)(T(*)(T t))typeof_f<T>;
+    funcptr_t type = (funcptr_t)(void(*)(T))typeof_f<T>;
+    template<typename OT> bool operator==(const OT& other){ return type == other.type; }
+    template<typename OT> bool operator==(OT&& other){ return type == other.type; }
+    template<funcptr_t> bool operator==(const funcptr_t& other){ return type == other; }
+    template<funcptr_t> bool operator==(funcptr_t&& other){ return type == other; }
+    template<typename OT> bool operator!=(const OT& other){ return type != other.type; }
+    template<typename OT> bool operator!=(OT&& other){ return type != other.type; }
+    template<funcptr_t> bool operator!=(const funcptr_t& other){ return type != other; }
+    template<funcptr_t> bool operator!=(funcptr_t&& other){ return type != other; }
+};
+
+template<typename T>
+void nulltypeof_f(){}
+
+template<>
+struct typeof<nullptr_t>
+{
+    funcptr_t type = (funcptr_t)(void(*)())nulltypeof_f<nullptr_t>;
     template<typename OT> bool operator==(const OT& other){ return type == other.type; }
     template<typename OT> bool operator==(OT&& other){ return type == other.type; }
     template<funcptr_t> bool operator==(const funcptr_t& other){ return type == other; }
@@ -50,17 +71,17 @@ struct lakType
     template<typename T>
     lakType(const T& other)
     {
-        type = typeof<remove_reference<T>::type>().type;
-        value = make_shared<remove_reference<T>::type>();
-        remove_reference<T>::type& ot = *(remove_reference<T>::type*)value.get();
+        type = typeof<remove_reference_t<T>>().type;
+        value = make_shared<remove_reference_t<T>>();
+        remove_reference_t<T>& ot = *(remove_reference_t<T>*)value.get();
         ot = other;
     }
     template<typename T>
     lakType(T&& other)
     {
-        type = typeof<remove_reference<T>::type>().type;
-        value = make_shared<remove_reference<T>::type>();
-        remove_reference<T>::type& ot = *(remove_reference<T>::type*)value.get();
+        type = typeof<remove_reference_t<T>>().type;
+        value = make_shared<remove_reference_t<T>>();
+        remove_reference_t<T>& ot = *(remove_reference_t<T>*)value.get();
         ot = other;
     }
     lakType& operator=(const lakType& other)
@@ -78,30 +99,30 @@ struct lakType
     template<typename T>
     lakType& set(const T& t)
     {
-        type = typeof<remove_reference<T>::type>().type;
-        value = make_shared<remove_reference<T>::type>();
-        remove_reference<T>::type& ot = *(remove_reference<T>::type*)value.get();
+        type = typeof<remove_reference_t<T>>().type;
+        value = make_shared<remove_reference_t<T>>();
+        remove_reference_t<T>& ot = *(remove_reference_t<T>*)value.get();
         ot = t;
         return *this;
     }
     template<typename T>
     lakType& set(T&& t)
     {
-        type = typeof<remove_reference<T>::type>().type;
-        value = make_shared<remove_reference<T>::type>();
-        remove_reference<T>::type& ot = *(remove_reference<T>::type*)value.get();
+        type = typeof<remove_reference_t<T>>().type;
+        value = make_shared<remove_reference_t<T>>();
+        remove_reference_t<T>& ot = *(remove_reference_t<T>*)value.get();
         ot = t;
         return *this;
     }
     template<typename T>
     T& get()
     {
-        return *(remove_reference<T>::type*)value.get();
+        return *(remove_reference_t<T>*)value.get();
     }
     template<typename T>
     T& getStrict()
     {
-        if (type == (funcptr_t)typeof<remove_reference<T>::type>().type) return get<T>();
+        if (type == typeof<remove_reference_t<T>>().type) return get<T>();
         else throw lak_type_exception();
     }
     template<typename T> bool operator==(const typeof<T>& tpo){ return type == tpo.type; }
